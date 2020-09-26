1d04b2c9fdaad8784a61792df0f53628cafa70e3
Robbe Van Herck <robbe@robbevanherck.be>
2020-09-26 14:23:02 +0200

    Show username in svg

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 online = None
+username = ""
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     global online
+    global username
     if request.method == "GET":
         if (online is None):
             path = "onoff.svg"
@@ -13,11 +15,14 @@ def index():
             path = "on.svg"
         else:
             path = "off.svg"
-        return app.send_static_file(path)
+        return render_template(path, username=username)
     elif request.method == "POST":
         data = request.json()
         if "trigger_word" not in data:
             return "no trigger word found"
+        if "user_name" not in data:
+            return "no username found"
+        username = data["user_name"]
         if data["trigger_word"].lower() == "on":
             online = True
         if data["trigger_word"].lower() == "off":
@@ -27,9 +32,12 @@ def index():
 @app.route("/toggle")
 def toggle():
     global online
+    global username
     if online:
         online = False
+        username = "onbekend"
     else:
         online = True
+        username = "onbekend"
     return str(online)
 
