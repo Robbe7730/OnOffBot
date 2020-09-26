b707039293bfe974ff03a66a282dc73618306775
Robbe Van Herck <robbe@robbevanherck.be>
2020-09-26 14:43:22 +0200

    Cleanup and add token check

--- app.py
+++ app.py
@@ -1,43 +1,76 @@
+"""
+app: the flask app serving the svg and recieving webhooks
+"""
+import configparser
+
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-online = None
-username = ""
+config = configparser.ConfigParser()
+config.read('secrets.ini')
 
-@app.route("/", methods = ["GET", "POST"])
+ONLINE = None
+USERNAME = ""
+TOKEN = config['secrets']['webhook_token']
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    global online
-    global username
+    """
+    index: show the on/off/onoff svg depending on the current status
+    """
+    global ONLINE
+    global USERNAME
     if request.method == "GET":
-        if (online is None):
+        # Return the correct svg
+        if ONLINE is None:
             path = "onoff.svg"
-        elif (online):
+        elif ONLINE:
             path = "on.svg"
         else:
             path = "off.svg"
-        return render_template(path, username=username)
-    elif request.method == "POST":
+
+        # Fill in the username
+        return render_template(path, username=USERNAME)
+    if request.method == "POST":
+        # Get the POST-ed data
         data = request.json
-        if "trigger_word" not in data:
-            return "no trigger word found"
-        if "user_name" not in data:
-            return "no username found"
-        username = data["user_name"]
+
+        # Make sure the right fields exist
+        if ("trigger_word" not in data or
+                "user_name" not in data or
+                "token" not in data):
+            return "Missing some fields", 400
+
+        # Make sure the token matches
+        if data["token"] != TOKEN:
+            return "Invalid token", 401
+
+        # Set the username
+        USERNAME = data["user_name"]
+
+        # Set the status
         if data["trigger_word"].lower() == "on":
-            online = True
+            ONLINE = True
         if data["trigger_word"].lower() == "off":
-            online = False
-        return str("online")
+            ONLINE = False
+
+        # Return the new status (unused by Mattermost)
+        return str(ONLINE)
+    return "This shouldn't be possible...", 400
+
 
 @app.route("/toggle")
 def toggle():
-    global online
-    global username
-    if online:
-        online = False
-        username = "onbekend"
+    """
+    toggle: for debugging only, toggles the on/off state
+    """
+    global ONLINE
+    global USERNAME
+    if ONLINE:
+        ONLINE = False
+        USERNAME = "onbekend"
     else:
-        online = True
-        username = "onbekend"
-    return str(online)
-
+        ONLINE = True
+        USERNAME = "onbekend"
+    return str(ONLINE)
