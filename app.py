71f82bd2d7f3afd0627c26b7621ebd5327e73132
Robbe Van Herck <robbe@robbevanherck.be>
2020-09-26 11:31:45 +0200

    Add POST logic for mattermost triggers

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 online = None
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
     global online
-    if (online is None):
-        path = "onoff.svg"
-    elif (online):
-        path = "on.svg"
-    else:
-        path = "off.svg"
-    return app.send_static_file(path)
+    if request.method == "GET":
+        if (online is None):
+            path = "onoff.svg"
+        elif (online):
+            path = "on.svg"
+        else:
+            path = "off.svg"
+        return app.send_static_file(path)
+    elif request.method == "POST":
+        data = request.json()
+        if "trigger_word" not in data:
+            return "no trigger word found"
+        if data["trigger_word"].lower() == "on":
+            online = True
+        if data["trigger_word"].lower() == "off":
+            online = False
+        return str("online")
 
 @app.route("/toggle")
 def toggle():
